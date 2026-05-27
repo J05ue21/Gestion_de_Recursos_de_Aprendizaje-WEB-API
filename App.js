6b69da8b34deffac8b644f3beadd3685b279b4c4
J05ue21 <orellana3566@gmail.com>
2026-05-26 19:42:11 -0600

    estilo: mejora en los botones del Tab.Navigator

--- App.js
+++ App.js
@@ -4,6 +4,21 @@ import { PaperProvider } from 'react-native-paper';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigation from './src/navigation/AppNavigation';
 
+import iconFont from 'react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf';
+const iconFontStyles = `@font-face { src: url(${iconFont}); font-family: MaterialCommunityIcons; }`;
+
+// Crear la etiqueta de estilo para inyectar la fuente en el navegador
+if (typeof document !== 'undefined') {
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = iconFontStyles;
+  } else {
+    style.appendChild(document.createTextNode(iconFontStyles));
+  }
+  document.head.appendChild(style);
+}
+
 export default function App() {
   return (
     <AuthProvider>
