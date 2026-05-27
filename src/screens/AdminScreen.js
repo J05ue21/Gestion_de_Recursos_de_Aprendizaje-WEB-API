592621bcef2a20fc94023da8ae4f0e7e6020eba7
J05ue21 <orellana3566@gmail.com>
2026-05-27 16:25:18 -0600

    agregar: funcion para obtener recursos en AdminScreen

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,11 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Button } from 'react-native-paper';
-import { AuthContext } from '../context/AuthContext';
+import { useFocusEffect } from '@react-navigation/native';
+import api from '../services/api'
 
 export default function AdminScreen() {
-  const { user, logout } = useContext(AuthContext);
-  
+  const [recursos, setRecursos] = useState([]);
+  const [loading, setLoading] = useState(true);  
+
+  // estados para el Formulario (Crear / Editar)
+  const [visibleDialog, setVisibleDialog] = useState(false);
+  const [editandoId, setEditandoId] = useState(null); // null para Crear; si hay ID, entoncer es Editar
+
+  const [titulo, setTitulo] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [imagen, setImagen] = useState('');
+  const [tipo, setTipo] = useState('Libro');
+  const [enlace, setEnlace] = useState('');
+
+  // se cargan los recursos de la API
+    const fetchRecursos = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get('/recursos');
+        setRecursos(response.data || []);
+      } 
+      catch (error) {
+        console.error("Hubo un error al cargar recursos en AdminScreen:", error);
+      }
+      finally {
+        setLoading(false);
+      }
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+          fetchRecursos();
+        }, [])
+      );
+      
+  //--------------------------------------------------------------
   return (
     <View style={styles.container}>
       <Text variant="headlineMedium">Administrar Recursos</Text>
